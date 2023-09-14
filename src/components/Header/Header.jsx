7e97dcc7ce951c7e7a486c819aa3b115e3ef4141
mrana007 <rana.es.bc@gmail.com>
2023-09-14 23:39:55 +0600

    added header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-4xl text-center pt-12 ">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
